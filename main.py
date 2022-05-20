f0a1cb460b1fc592e881051295081267c9a6f55e
A5TA <cardillojamesbrandon@gmail.com>
2022-05-20 16:15:20 -0400

    added a while loop to add txt file stuff to the google sheets

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import gspread
+import time
 import globals
 # from twitter import *
-numberOfCOmments = 20
+numberOfComments = 20
 # t = Twitter(
 #     auth=OAuth(globals.token, globals.token_secret, globals.consumer_key, globals.consumer_secret))
 
@@ -10,7 +11,29 @@ gc = gspread.service_account('credentials.json') #Paste your credentials in a cr
 # Open a sheet from a spreadsheet in one go
 wks = gc.open("RookieRobot").sheet1 #Here I put RookieRobot because that is the name of my sheets file 
 
-# Update a range of cells using the top left corner address
+# Update the speadsheet with the quotes
+
+'''Uncomment this code segment to copy over the contents of the txt file to your google sheets
 column = 'A'
-for row in range(numberOfCOmments):
-    wks.update(str(column)+(str(row+1)), [['Hello '+ str(row+1)]])
+row = 1
+file = open('quotes.txt','r', encoding='utf-8') #open the file
+line = file.readline() #read the first line
+quote = [] #Use a list to append quotes to the spreadsheet
+
+while line != "ENDofFile":
+    if line == "\n":
+        x = ' '.join(quote)
+        print(x)
+        try:
+            wks.update(str(column)+(str(row)), [[x]]) 
+            time.sleep(1)
+        except:
+            time.sleep(10)
+            wks.update(str(column)+(str(row)), [[x]]) 
+        row += 1
+        quote.clear() #clear the list for the next quote
+    else:
+        quote.append(line.strip("\n"))
+    line = file.readline()
+file.close()
+'''
