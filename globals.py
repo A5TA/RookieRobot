fed0f65dbd94201973cd9cde9c18a12d6020bc1d
Brandon <84144584+A5TA@users.noreply.github.com>
2022-05-21 17:11:04 -0400

    Update globals.py

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAEBDcwEAAAAAN1zXNfKqQy4AVs0jBI6GF0jwWsU%3DSyDFu2CmKZBTofSJhXXdsz6iwmGSg8E7M2uJ6Ft50AeGKusU1b" #Bearer token
-token = "1527349348916351003-LzNteeZCXgUUF57kR2V76lULzwXBfM" #Access token
-token_secret = "zjqS2a7AUigghIoYPFle9YVmLYesuOPOoSlbauwoss4dW" #Access token secret
-consumer_key = "hlm2rHFQcwPQvllQ1NmkF5gZQ" #API KEY
-consumer_secret = "KuKm7SgiWBO62afr1KemUac74RX13ryBphwDfvCBwnR30iuZJA" #API KEY SECRET
+bearer_token = "" #Bearer token
+token = "" #Access token
+token_secret = "" #Access token secret
+consumer_key = "" #API KEY
+consumer_secret = "" #API KEY SECRET
